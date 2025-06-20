88b0a74ea8dea0965ae953c51fe3663a32a3ffcd
ngobesea <andilekngobese@gmail.com>
2025-06-20 15:57:08 +0200

    adding login and user creation functions

---
+++ event_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import login_view, register_view, home_view
+
+urlpatterns = [
+    path('', home_view, name="login"),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+]
--- event_app/views.py
+++ event_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request.POST)
+        if login_form.is_valid():
+            user = authenticate(request, login_form)
+            login(request, user)
+            return redirect('home')
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'form': login_form})
+
+def register_view(request):
+    '''
+        Create a user account for people who are responsible to post events
+    '''
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            return redirect('home')
+    else:
+        register_form = UserCreationForm()
+    return render(request, 'registration.html', {'form': register_form})
+
